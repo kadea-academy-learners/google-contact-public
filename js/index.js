8ad9471781cdf5abe9e4a91c9d5374a84cf353e9
ChrisMwanya <cmwanya@gmail.com>
2024-05-08 16:31:42 +0100

    style: Update CSS styles for background, disposition, and header

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// Selects all elements
+
+const burgerMenu = document.querySelector("#burger-menu");
+const sideBarMenu = document.querySelector("#side-bar");
+const mainSection = document.querySelector("#main-section");
+
+// Callback function
+
+const handleAddStyle = (element, style) => {
+  element.classList.add(style);
+};
+
+const handleRemoveStyle = (element, style) => {
+  element.classList.remove(style);
+};
+
+// Add event
+
+burgerMenu.addEventListener("click", (event) => {
+  if (sideBarMenu.classList.contains("display-none")) {
+    handleRemoveStyle(sideBarMenu, "display-none");
+    handleRemoveStyle(mainSection, "w-100");
+  } else {
+    handleAddStyle(sideBarMenu, "display-none");
+    handleAddStyle(mainSection, "w-100");
+  }
+});
